e4f3788805c8f404a90d86e39a269ef2aada4b2b
Alfonso Jimenez <djalfo18@gmail.com>
2024-01-26 12:49:25 -0500

    Applying global styles in the stories with decorators

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,6 +3,27 @@ import 'minireset.css'
 import '../styles/global.css'
 import '../styles/tokens.css'
 
+const styles = {
+  display: "flex",
+  flexDirection: "column",
+  maxHeight: "auto",
+  justifyContent: "flex-start",
+  alignContent: "flex-start",
+  flexWrap: "wrap",
+  height: "100%",
+  gap: "10px 30px",
+  background: "white",
+  padding: "20px"
+}
+
+export const decorators = [
+  (Story) => (
+    <div style={styles}>
+      <Story />
+    </div>
+  )
+]
+
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
