241a931b865d164b2aff9b5019c6020c42c22c6f
Alfonso Jimenez <djalfo18@gmail.com>
2024-01-30 12:56:12 -0500

    Wrapping stories on i18next provider

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,6 +4,8 @@ import '../styles/global.css'
 import '../styles/tokens.css'
 
 import i18n from './i18next';
+import React, { Suspense } from 'react';
+import { I18nextProvider } from 'react-i18next';
 
 const styles = {
   display: "flex",
@@ -18,13 +20,23 @@ const styles = {
   padding: "20px"
 }
 
-export const decorators = [
-  (Story) => (
-    <div style={styles}>
-      <Story />
-    </div>
-  )
-]
+// Wrap your stories in the I18nextProvider component
+const withI18next = (Story) => {
+  return (
+    // This catches the suspense from components not yet ready (still loading translations)
+    // Alternative: set useSuspense to false on i18next.options.react when initializing i18next
+    <Suspense fallback={<div>loading translations...</div>}>
+      <I18nextProvider i18n={i18n}>
+        <div style={styles}>
+          <Story />
+        </div>
+      </I18nextProvider>
+    </Suspense>
+  );
+};
+
+// export decorators for storybook to wrap your stories in
+export const decorators = [withI18next];
 
 const preview = {
   globals: {
